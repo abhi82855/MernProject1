ba836f13ea1340fc808ff11fb59c37984063d2af
Nitish <nitishsingh990@gmail.com>
2021-08-24 09:40:01 +0530

    ui

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import Home from "./containers/Home";
 import Signin from "./containers/Signin";
 import Signup from "./containers/Signup";
+import Header1 from "./components/Header/header";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    aaaa
     <div className="App">
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/signin" exact component={Signin} />
           <Route path="/signup" exact component={Signup} />
+          <Route path="/front-end">
+            <Header1 />
+          </Route>
         </Switch>
       </Router>
     </div>
