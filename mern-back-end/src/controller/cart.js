6fe908b44e26ba6ca4259f08bb9bbaa3b5efe755
abhi82855 <abhi82855@gmail.com>
2021-08-27 14:02:10 +0530

    cart added

--- mern-back-end/src/controller/cart.js
+++ mern-back-end/src/controller/cart.js
@@ -1,30 +1,46 @@
 const Cart = require("../models/cart");
 
 exports.addItemToCart = (req, res) => {
-  Cart.find({ user: req.user._id }).exec((error, cart) => {
+  Cart.findOne({ user: req.user._id }).exec((error, cart) => {
     if (error) return res.status(400).json({ error });
     if (cart) {
       //Cart already exists then update
       //return res.status(400).json({ message: cart });
       //this query find existing and update
-
-      const isItemAdded = cart.cartItems.find(
-        (c) => c.product == req.body.cart.product
-      );
-
-      Cart.findOneAndUpdate(
-        { user: req.user._id },
-        {
-          $push: {
-            cartItems: req.body.cartItems,
-          },
-        }
-      ).exec((error, _cart) => {
-        if (error) return res.status(400).json({ error });
-        if (_cart) {
-          return res.status(201).json({ cart: _cart });
-        }
-      });
+      const product = req.body.cartItems.product;
+      const item = cart.cartItems.find((c) => c.product == product);
+      if (item) {
+        Cart.findOneAndUpdate(
+          { user: req.user._id, "cartItems.product": product },
+          {
+            $set: {
+              cartItems: {
+                ...req.body.cartItems,
+                quantity: item.quantity + req.body.cartItems.quantity,
+              },
+            },
+          }
+        ).exec((error, _cart) => {
+          if (error) return res.status(400).json({ error });
+          if (_cart) {
+            return res.status(201).json({ cart: _cart });
+          }
+        });
+      } else {
+        Cart.findOneAndUpdate(
+          { user: req.user._id },
+          {
+            $push: {
+              cartItems: req.body.cartItems,
+            },
+          }
+        ).exec((error, _cart) => {
+          if (error) return res.status(400).json({ error });
+          if (_cart) {
+            return res.status(201).json({ cart: _cart });
+          }
+        });
+      }
     } else {
       //if doesnt exists the make new cart
       const cart = new Cart({
