9690bcd1b268e575d16584b9739fdc37d478ff01
abhi82855 <abhi82855@gmail.com>
2021-09-16 11:18:33 +0530

    actions

--- admin-app/src/reducers/index.js
+++ admin-app/src/reducers/index.js
@@ -1,9 +1,15 @@
 import { combineReducers } from "redux";
 import authReducers from "./auth.reducers";
+import categoryReducer from "./category.reducer";
 import userReducer from "./user.reducer";
+import productReducer from "./product.reducer";
 
 const rootReducer = combineReducers({
   auth: authReducers,
   user: userReducer,
+  category: categoryReducer,
+  product: productReducer,
+  // order: orderReducer,
+  // page: pageReducer,
 });
 export default rootReducer;
--- mern-back-end/src/controller/admin/auth.js
+++ mern-back-end/src/controller/admin/auth.js
@@ -2,18 +2,19 @@ const User = require("../../models/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 exports.signup = (req, res) => {
-  User.findOne({ email: req.body.email }).exec((error, user) => {
+  User.findOne({ email: req.body.email }).exec(async (error, user) => {
     if (user)
       return res.status(400).json({
         message: "Admin already registered",
       });
 
     const { firstName, lastName, email, password } = req.body;
+    const hash_password = await bcrypt.hash(password, 10);
     const _user = new User({
       firstName,
       lastName,
       email,
-      password,
+      hash_password,
       username: Math.random().toString(),
       role: "admin",
     });
@@ -43,11 +44,11 @@ exports.signin = (req, res) => {
           { _id: user._id, role: user.role },
           process.env.JWT_SECRET,
           {
-            expiresIn: "3h",
+            expiresIn: "1d",
           }
         );
         const { _id, firstName, lastName, email, role, fullName } = user;
-        res.cookie("token", token, { expiresIn: "1h" });
+        res.cookie("token", token, { expiresIn: "1d" });
         res.status(200).json({
           token,
           user: { _id, firstName, lastName, email, role, fullName },
